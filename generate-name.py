263f7de49335cdacb20a5a6738e88d9bb50892a1
Tiantu Xu <xu944@purdue.edu>
2018-12-16 15:49:12 -0500

    update

--- generate-name.py
+++ generate-name.py
@@ -1,9 +1,9 @@
 import sys,os
 
-f = open("/tmp/darknet/data/train.txt", "w+")
+f = open("./images.txt", "w+")
 for i in range(1, 5001):
 		# Change this as needed!!!!
-		prefix = "/home/teddyxu/african/"
+		prefix = "/path/to/images/"
 		suffix = ".jpg"
 		#print prefix + str(i) + suffix
 		f.write(prefix + str(i).zfill(7) + suffix + "\n")
